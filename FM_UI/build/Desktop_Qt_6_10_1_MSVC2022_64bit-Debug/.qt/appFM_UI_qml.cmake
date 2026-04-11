da8b0c4c61ea1207d77141c3e66ce4d2f04ba56a
MertDemiroglu <egelimert2006@gmail.com>
2026-04-11 16:55:08 +0300

    Phase 1: remove tracked build artifacts from repository

--- FM_UI/build/Desktop_Qt_6_10_1_MSVC2022_64bit-Debug/.qt/appFM_UI_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appFM_UI")
-set(working_dir "C:/Users/user/Documents/FM_UI")
-set(src_and_dest_list
-    "C:/Users/user/Documents/FM_UI/Main.qml"
-    "C:/Users/user/Documents/FM_UI/build/Desktop_Qt_6_10_1_MSVC2022_64bit-Debug/FM_UI/Main.qml"
-
-)
-set(timestamp_file "C:/Users/user/Documents/FM_UI/build/Desktop_Qt_6_10_1_MSVC2022_64bit-Debug/.qt/appFM_UI_qml.txt")
